ddca906df3cfe83499c30b448dd92ced0939b1d1
jahangir <jahangir.droid@gmail.com>
2023-08-08 00:49:40 +0600

    script updated

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express();
 app.use('/', (req, res) => {
   res.send('Hello, I am finally running');
 });
-
 app.listen(4000, () => {
   console.log('server is running on port 4000!!!');
 });
